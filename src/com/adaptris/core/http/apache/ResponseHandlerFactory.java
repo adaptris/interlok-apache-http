d598d0ffa6783b1b9395fa5576abc2d4a063e97a
Lewin Chan <lewin.chan@adaptris.com>
2015-11-13 10:53:04 +0000

    INTERLOK-708 Added the same "write to metadata" feature to Apache-http INTERLOK-708 #time 45m
    Added a similar method to write to metadata for apache-http even though
    it doesn't use the DataINput/Output parameter stuff.

---
+++ src/com/adaptris/core/http/apache/ResponseHandlerFactory.java
@@ -0,0 +1,18 @@
+package com.adaptris.core.http.apache;
+
+import org.apache.http.client.ResponseHandler;
+
+import com.adaptris.core.AdaptrisMessage;
+
+/**
+ * Factory for creating a {@link ResponseHandler} for use with the {@link
+ * org.apache.http.client.HttpClient#execute(org.apache.http.client.methods.HttpUriRequest, ResponseHandler))} method.
+ * 
+ * @author lchan
+ *
+ */
+public interface ResponseHandlerFactory {
+
+  ResponseHandler<AdaptrisMessage> createResponseHandler(HttpProducer owner);
+
+}
