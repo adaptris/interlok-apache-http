f290a2eace539c7ff5d4d39509e5c1f58b3f60c8
Sebastien Belin <sebastien.belin@adaptris.com>
2017-02-02 02:21:29 +0000

    INTERLOK-1395 Fix dependency

--- test/src/com/adaptris/core/http/apache/JettyHelper.java
+++ test/src/com/adaptris/core/http/apache/JettyHelper.java
@@ -21,7 +21,7 @@ import com.adaptris.core.http.jetty.MessageConsumer;
 import com.adaptris.core.http.jetty.ResponseProducer;
 import com.adaptris.core.management.webserver.SecurityHandlerWrapper;
 import com.adaptris.core.http.server.HttpStatusProvider.HttpStatus;
-import com.adaptris.core.http.server.MetadataHeaderHandler;
+import com.adaptris.core.http.jetty.MetadataHeaderHandler;
 import com.adaptris.core.stubs.MockChannel;
 import com.adaptris.core.stubs.MockMessageProducer;
 import com.adaptris.core.util.LifecycleHelper;
