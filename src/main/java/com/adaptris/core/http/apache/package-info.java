3cfb652c94c426910b6ae0f7d5a47985e333300b
Lewin Chan <lewin.chan@adaptris.com>
2018-07-05 10:20:44 +0100

    Add UML package

--- src/main/java/com/adaptris/core/http/apache/package-info.java
+++ src/main/java/com/adaptris/core/http/apache/package-info.java
@@ -1,5 +1,9 @@
 /**
- * Implementation of the core {@link com.adaptris.core.RequestReplyProducerImp} that uses the <a
- * href="http://hc.apache.org/index.html">Apache HTTP Client</a> as the underlying transport.
+ * <p>
+ * Alternative HTTP client Implementations for Interlok that uses <a href="http://hc.apache.org/index.html">Apache HTTP Client</a>
+ * as the underlying transport.
+ * </p>
+ * <img alt="UML" src="package.svg"/>
+ * 
  */
 package com.adaptris.core.http.apache;
